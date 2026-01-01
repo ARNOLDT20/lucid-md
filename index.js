a19d5b434dfd24640aa4f54a8279edf43f67854c
ARNOLD <atarimo117@gmail.com>
2025-12-31 17:31:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -227,12 +227,9 @@ async function connectToWA() {
         }
       }
       const q = args.join(' ')
-      const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => { }) : ''
+      let groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => { }) : ''
       const groupName = isGroup ? groupMetadata.subject : ''
-      const participants = isGroup ? await groupMetadata.participants : ''
-      const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => { }) : ''
-      const groupName = isGroup ? groupMetadata.subject : ''
-      const participants = isGroup ? await groupMetadata.participants : ''
+      const participants = isGroup ? groupMetadata.participants : ''
       const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
       const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
       const isAdmins = isGroup ? groupAdmins.includes(sender) : false
